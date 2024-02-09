98aa7376f0c504882effa0177a3f3315bf3a3f3d
Roland Krancz <roland1.krancz@bbraun.com>
2024-02-09 08:27:13 +0100

    Succesfully generated dll..

--- BL/src/VehicleInteractor.h
+++ BL/src/VehicleInteractor.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#ifdef DLL_EXPORT
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT __declspec(dllimport)
+#endif
+
 #include "VehicleEntity.h"
 #include "IStorage.h"
 #include "IVehicleManager.h"
 #include "IPresenterApi.h"
 
-class VehicleInteractor : public IVehicleManager {
+class DLL_EXPORT VehicleInteractor : public IVehicleManager {
 private:
     IStorage* storage;
     IPresenterApi* presenter;
