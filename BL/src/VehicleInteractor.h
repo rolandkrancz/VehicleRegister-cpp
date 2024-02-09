ec2e49e20d80c5ed0d9bdc8e335d527a62ed2793
Roland Krancz <roland1.krancz@bbraun.com>
2024-02-09 08:51:05 +0100

    Change preprocessor defines

--- BL/src/VehicleInteractor.h
+++ BL/src/VehicleInteractor.h
@@ -1,9 +1,11 @@
 #pragma once
 
-#ifdef DLL_EXPORT
-#define DLL_EXPORT __declspec(dllexport)
+#if defined(DLL_EXPORT)
+#define DLL __declspec(dllexport)
+#elif defined(DLL_IMPORT)
+#define DLL __declspec(dllimport)
 #else
-#define DLL_EXPORT __declspec(dllimport)
+#define DLL
 #endif
 
 #include "VehicleEntity.h"
@@ -11,7 +13,7 @@
 #include "IVehicleManager.h"
 #include "IPresenterApi.h"
 
-class DLL_EXPORT VehicleInteractor : public IVehicleManager {
+class DLL VehicleInteractor : public IVehicleManager {
 private:
     IStorage* storage;
     IPresenterApi* presenter;
