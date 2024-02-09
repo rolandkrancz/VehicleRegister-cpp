1dd69b880bf594513092411e6e291417d0b74952
Roland Krancz <roland1.krancz@bbraun.com>
2024-02-09 11:08:29 +0100

    add missing file

---
+++ UI/src/VehicleViewModel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+typedef struct VehicleViewModel {
+    std::string registrationNumber;
+    std::string model;
+    std::string vehicleType;
+    std::string firstName;
+    std::string address;
+} VehicleViewModel;
